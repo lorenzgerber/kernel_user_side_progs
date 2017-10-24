cb35a7605ef52961fbe7c51799d537f8c9465099
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-10-24 22:49:40 +0200

    test thread pid

--- kvs_get.c
+++ kvs_get.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define NETLINK_USER 31
 #define PUT 0
@@ -43,7 +44,7 @@ int main(int argc, char*argv[]) {
 
 	memset(&src_addr, 0, sizeof(src_addr));
 	src_addr.nl_family = AF_NETLINK;
-	src_addr.nl_pid = getpid(); /* self pid */
+	src_addr.nl_pid = pthread_self() << 16 | getpid();
 
 	bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
 
@@ -55,7 +56,7 @@ int main(int argc, char*argv[]) {
 	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
 	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
 	nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
-	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_pid = pthread_self() << 16 | getpid();
 	nlh->nlmsg_flags = 0;
 
 
