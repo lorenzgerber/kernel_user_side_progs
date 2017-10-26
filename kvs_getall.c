dc2db053a7df7b8f31afaca8034b9547d6b3214e
pajasson <1982konig@gmail.com>
2017-10-26 20:23:26 +0200

    making sure user get anything at all

--- kvs_getall.c
+++ kvs_getall.c
@@ -81,7 +81,7 @@ int main(int argc, char*argv[]) {
 
 	/* Read message from kernel */
 	recvmsg(sock_fd, &msg, 0);
-	printf("Received message payload: %s\n", ((struct keyvalue *)NLMSG_DATA(nlh))->value);
+	//printf("Received message payload: %s\n", ((struct keyvalue *)NLMSG_DATA(nlh))->value);
 
 	close(sock_fd);
 }
