6e152a39077c587d3edaffcb8ade56013cc3c10c
pajasson <1982konig@gmail.com>
2017-10-28 20:44:32 +0200

    restore untested, also adds testvalues if backupfile is empty

--- kvs_restore.c
+++ kvs_restore.c
@@ -4,21 +4,36 @@
  *  Created on: Oct 28, 2017
  *      Author: knig
  */
+#define MAX_PAYLOAD 1024
+#define NETLINK_USER 31
+#define PUT 0
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
+
+struct keyvalue {
+	int operation;
+	int key;
+	char *value;
+};
 
 struct keyvalue_pair{
 	int key;
 	char data[];
 };
-int fsize(FILE *fp){
-    int prev=ftell(fp);
-    fseek(fp, 0L, SEEK_END);
-    int sz=ftell(fp);
-    fseek(fp,prev,SEEK_SET); //go back to where we were
-    return sz;
-}
+
+
+int fsize(FILE *fp);
+int restore(int key, char* value);
 int main(int argc, char* argv[]){
 
 	char c;
@@ -79,4 +94,65 @@ int main(int argc, char* argv[]){
 	free(data);
 	fclose(fp);
 	return 0;
+}
+
+int restore(int key, char* value){
+
+	struct keyvalue *data;
+
+	sock_fd=socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+	if(sock_fd<0)
+		return -1;
+
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pid = getpid(); /* self pid */
+
+	bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.nl_family = AF_NETLINK;
+	dest_addr.nl_pid = 0; /* For Linux Kernel */
+	dest_addr.nl_groups = 0; /* unicast */
+
+	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+	nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_flags = 0;
+
+
+	data = malloc(sizeof(struct keyvalue));
+	data->key = key;
+	data->operation = 0;
+	data->value = malloc(sizeof(char)*strlen(value)+1);
+	strcpy(data->value, value);
+	memcpy(NLMSG_DATA(nlh), data, sizeof(struct keyvalue));
+
+
+	iov.iov_base = (void *)nlh;
+	iov.iov_len = nlh->nlmsg_len;
+	msg.msg_name = (void *)&dest_addr;
+	msg.msg_namelen = sizeof(dest_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	printf("Sending message \"%s\" to kernel\n", data->value);
+	sendmsg(sock_fd,&msg,0);
+	printf("Waiting for message from kernel\n");
+
+	/* Read message from kernel */
+	recvmsg(sock_fd, &msg, 0);
+	printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
+
+	close(sock_fd);
+	return 0;
+}
+
+int fsize(FILE *fp){
+    int prev=ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    int sz=ftell(fp);
+    fseek(fp,prev,SEEK_SET); //go back to where we were
+    return sz;
 }
