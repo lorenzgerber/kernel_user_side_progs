90f042297002ef83ef6939b9a3e94601d951179d
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-10-28 14:01:03 +0200

    testing with opcodes

--- kvs_get.c
+++ kvs_get.c
@@ -79,7 +79,7 @@ int main(int argc, char*argv[]) {
 
 	/* Read message from kernel */
 	recvmsg(sock_fd, &msg, 0);
-	printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
+	printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh)+1);
 
 	close(sock_fd);
 }
